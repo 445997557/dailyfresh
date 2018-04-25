38290d299b826d327874aea9bc98e4dafd20bd5d
wenhao <wenhao@163.com>
2018-04-26 00:02:48 +0800

    04-2.首页动态显示(celery)

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.goods.models import *
+
+from celery_tasks.tasks import *
+
+
+class BaseAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        print('save_model={}'.format(obj))
+        generate_static_index_page.delay()
+
+    def delete_model(self, request, obj):
+        super().delete_model(request, obj)
+        print('delete_model={}'.format(obj))
+        generate_static_index_page.delay()
+
+
+class GoodsCategoryAdmin(BaseAdmin):
+    pass
+
+
+class GoodsSPUAdmin(BaseAdmin):
+    pass
+
+
+class GoodsSKUAdmin(BaseAdmin):
+    pass
+
+
+class IndexSlideGoodsAdmin(BaseAdmin):
+    pass
+
+
+class IndexCategoryGoodsAdmin(BaseAdmin):
+    pass
+
+
+class IndexPromotionAdmin(BaseAdmin):
+    pass
+
+
+# class GoodsImageAdmin(BaseAdmin):
+#     pass
+
+
+admin.site.register(GoodsCategory, GoodsCategoryAdmin)
+admin.site.register(GoodsSPU, GoodsSPUAdmin)
+admin.site.register(GoodsSKU, GoodsSKUAdmin)
+admin.site.register(IndexSlideGoods, IndexSlideGoodsAdmin)
+admin.site.register(IndexCategoryGoods, IndexCategoryGoodsAdmin)
+admin.site.register(IndexPromotion, IndexPromotionAdmin)
+# admin.site.register(GoodsImage, GoodsImageAdmin)
--- celery_tasks/tasks.py
+++ celery_tasks/tasks.py
@@ -1,6 +1,17 @@
+# import os
+# import django
+# # 设置环境变量
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dailyfresh.settings")
+# # 初始化django环境
+# django.setup()
+from time import sleep
+
 from celery import Celery
 from django.core.mail import send_mail
+from django.template import loader
+from apps.goods.models import *
 from dailyfresh import settings
+
 
 app = Celery('celery_tasks.tasks', broker='redis://127.0.0.1:6379/1')
 
@@ -21,3 +32,30 @@ def send_active_mail(username, email, token):
               from_email=from_email,
               recipient_list=recipient_list,
               html_message=html_message)
+
+
+@app.task
+def generate_static_index_page():
+    sleep(2)
+    categories = GoodsCategory.objects.all()
+    slide_skus = IndexSlideGoods.objects.all().order_by('index')
+    promotions = IndexPromotion.objects.all().order_by('index')[0:2]
+    for c in categories:
+        text_skus = IndexCategoryGoods.objects.filter(display_type=0, category=c)
+        image_skus = IndexCategoryGoods.objects.filter(display_type=1, category=c)[0:4]
+        c.text_skus = text_skus
+        c.image_skus = image_skus
+
+    cart_count = 0
+
+    context = {
+        'categories': categories,
+        'slide_skus': slide_skus,
+        'promotions': promotions,
+        'cart_count': cart_count
+    }
+    template = loader.get_template('index.html')
+    html_str = template.render(context)
+    path = '/home/python/Documents/static/index.html'
+    with open(path, 'w') as file:
+        file.write(html_str)
